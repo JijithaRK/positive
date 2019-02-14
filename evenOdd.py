ad8f5d53bd82a5b89fe1209b4c0a432679587b12
JijithaRK <47183088+JijithaRK@users.noreply.github.com>
2019-02-14 20:31:34 +0530

    Update evenOdd.py

--- evenOdd.py
+++ evenOdd.py
@@ -1,8 +1,8 @@
- edit  fork	 download  copy
-j1 = float(input())
-if j1 > 0:
-   print("Positive")
-elif j1 == 0:
-   print("Zero")
+ j1=int(input())
+if (j1>0):
+    if(j1%2==0):
+        print("Even")
+    else:
+        print("Odd")
 else:
-   print("Negative")
+    print("invalid")
